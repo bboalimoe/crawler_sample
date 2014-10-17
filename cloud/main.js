0c7d9d2b724fa6901f9b9acf73375b9ce182f4bb
Xiaoping Feng <fxp007@gmail.com>
2014-10-17 19:25:24 +0800

    wrap message queue

--- cloud/main.js
+++ cloud/main.js
@@ -9,8 +9,15 @@ var MQController = function (queueName, doTask) {
     var iron_mq = require('iron_mq'),
         imq = new iron_mq.Client({
             token: "Vycchg8gKy52kHA9BAKSVcWc9QQ",
-            project_id: "543f8532d608d100090000a1"
+            project_id: "543f8532d608d100090000a1",
+            queue: queueName
         }),
+        exceptionImq = new iron_mq.Client({
+            token: "Vycchg8gKy52kHA9BAKSVcWc9QQ",
+            project_id: "543f8532d608d100090000a1",
+            queue: "failedTask"
+        }),
+        exceptionQueue = exceptionImq.queue('failedTask'),
         queue = imq.queue(queueName),
         deleteJob = function (job) {
             queue.del(job.id, function (error, body) {
@@ -26,17 +33,27 @@ var MQController = function (queueName, doTask) {
         success = function (job) {
             console.log('task success');
             deleteJob(job);
+        },
+        exception = function (job) {
+            console.log('task exception');
+            exceptionQueue.post(job, function (error, body) {
+                console.log('task moved to failedTask,oldId=%s,newId=%s', job.id, body);
+            });
+            deleteJob(job);
         };
-
 
     function fetchTask() {
         queue.get({}, function (error, body) {
             if (body) {
-                doTask(body, function () {
-                    success(body);
-                }, function () {
-                    failed(body);
-                });
+                try {
+                    doTask(body, function () {
+                        success(body);
+                    }, function () {
+                        failed(body);
+                    });
+                } catch (err) {
+                    exception(body);
+                }
                 fetchTask();
             } else {
                 setTimeout(fetchTask, 5000);
@@ -52,8 +69,11 @@ var MQController = function (queueName, doTask) {
     }
 }
 
+
 var controller = new MQController("crawler_sample", function (task, success, failed) {
-    console.log('work work,%s', JSON.stringify(task));
+    console.log('work work');
+    console.dir(JSON.parse(task.body));
+    // Do something
     success();
 });
 controller.start();
