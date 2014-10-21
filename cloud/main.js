b124016c4d9ab0d2721985dfe009d69d6df6509b
heamon7 <zhanghengyang@zhanghengyangdeMacBook-Air.local>
2014-10-21 21:14:06 +0800

    21:09

--- cloud/main.js
+++ cloud/main.js
@@ -13,7 +13,7 @@ var ContextLoader = function (appId, appKey) {
  this.AV.initialize(appId, appKey);
  }
  this.loadContext = function (appId, appKey) {
- var code = fs.readFileSync('./cloud/av.js', {encoding: 'utf8'});
+ var code = fs.readFileSync('cloud/av.js', {encoding: 'utf8'});
  this.script = code;
  this.runInThis.call(this);
  }
@@ -23,7 +23,7 @@ var ContextLoader = function (appId, appKey) {
  var techhackLoader =new ContextLoader('xv1cgfapsn90hyy2a42i9q6jg7phbfmdpt1404li6n93tt2r','70sp4h8prccxzyfp56vwm9ksczji36bsrjvtwzvrzegfza67')
 
 // input "websiteUrl" to crawling the  website and callback its websiteBody
-techhackLoader.AV.Cloud.run("hello", {name: 'dennis'}, {
+techhackLoader.AV.Cloud.run("crawlingWebsite", {websiteUrl: 'http://www.producthunt.com/'}, {
     success: function(data){
         //调用成功，得到成功的应答data
         console.log(data);
